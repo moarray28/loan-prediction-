f70a817dec19c9b47cc35f29b400f3116384756f
Ritesh More <111306175+moarray28@users.noreply.github.com>
2026-06-09 08:37:20 +0000

    reformatting code

--- backend/app/api/v1/prediction.py
+++ backend/app/api/v1/prediction.py
@@ -5,30 +5,19 @@ from app.models.request import LoanPredictionRequest
 from app.models.response import LoanPredictionResponse
 
 from app.db.dependencies import get_db
+from app.db.models import Prediction
 from app.services.prediction_service import save_prediction
 
 router = APIRouter()
-router = APIRouter()
-
 
 @router.get("/")
 def prediction_root():
     return {"message": "Prediction API V1"}
 
-
 @router.post(
     "/",
     response_model=LoanPredictionResponse
 )
-
-@router.get("/history")
-def get_history(
-    db: Session = Depends(get_db)
-):
-    predictions = db.query(Prediction).all()
-
-    return predictions
-
 def predict(
     request: LoanPredictionRequest,
     db: Session = Depends(get_db)
@@ -46,3 +35,25 @@ def predict(
         approval_probability=0.85,
         lead_priority="High"
     )
+
+
+@router.get("/history")
+def get_history(
+    db: Session = Depends(get_db)
+):
+    predictions = db.query(Prediction).all()
+
+    result = []
+
+    for p in predictions:
+        result.append({
+            "id": p.id,
+            "income": p.income,
+            "loan_amount": p.loan_amount,
+            "credit_score": p.credit_score,
+            "risk": p.risk,
+            "approval_probability": p.approval_probability,
+            "lead_priority": p.lead_priority
+        })
+
+    return result
